6cb31bc7a8b48bd7674826a7e90d2a8e283ae398
oussou <oussou.dev@gmail.com>
2018-12-29 15:52:31 +0000

    Add styles to Search.js

--- Components/Search.js
+++ Components/Search.js
@@ -1,16 +1,33 @@
+// import * as React from 'react';
+// Components/Search.js
+
 import React from 'react'
 import { View, TextInput, Button } from 'react-native'
 
 class Search extends React.Component {
   render() {
     return (
-      
       <View>
         <TextInput placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={() => {}}/>
+        <Button style={{height: 50}}title='Rechercher' onPress={() => {}}/>
       </View>
     )
   }
 }
 
+const styles = {
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+
+  }
+
+}
+
+
 export default Search 
+
