9f2240680b1a68e191716d151bf02996505db7ca
oussou <oussou.dev@gmail.com>
2019-01-04 21:37:05 +0000

    Add uri image to Image component

--- Components/Search.js
+++ Components/Search.js
@@ -6,29 +6,44 @@ import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
 class Search extends React.Component {
   constructor(props) {
     super(props)
-    this._films = []
+    //this._films = []
+    this.searchedText = ''
     this.state = {films: []}
   }
 
   _loadFilms() {
-      
-     getFilmsFromApiWithSearchedText('star').then(data => {
+    if(this.searchedText.length > 0) {
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
        this.setState({films: data.results})
     })
-    
+    }
+  }
+
+  _searchTextInputChanged(text) {
+    // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+    this.searchedText = text
+  }
+
     /*
     getFilmsFromApiWithSearchedText('star').then(data => {
       this._films = data.results
       this.forceUpdate()
     })
     */
-  }
 
   render() {
     return (
       <View style={styles.main_container}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'/>
-        <Button style={{height: 50}} title='Rechercher' onPress={() => this._loadFilms()}/>
+        <TextInput 
+          style={styles.textinput} 
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button 
+          style={{height: 50}} 
+          title='Rechercher' 
+          onPress={() => this._loadFilms()}
+        />
       
       <FlatList 
         // data={this._films}
