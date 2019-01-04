582ea7666e40c844f1a5d068cb444ad7f1dbd9a7
oussou <oussou.dev@gmail.com>
2019-01-04 16:43:27 +0000

    fetch url from TMDB

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,3 +1,12 @@
 // API/TMDBApi.js
 
+/* global fetch */
+
 const API_TOKEN = "a0d7733d8434b8d04f336873a61f9f09";
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text;
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
