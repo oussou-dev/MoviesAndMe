452b75dd138a12663719311957e429117f891d9c
oussou <oussou.dev@gmail.com>
2019-01-10 08:40:35 +0000

    Add Store to App

--- App.js
+++ App.js
@@ -2,11 +2,15 @@
 
 import React from 'react'
 import Navigation from './Navigation/Navigation'
+import { Provider } from 'react-redux'
+import Store from './Store/configureStore'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Navigation />
+      <Provider Store={Store}>
+        <Navigation/>
+      </Provider>
     )
   }
 }
